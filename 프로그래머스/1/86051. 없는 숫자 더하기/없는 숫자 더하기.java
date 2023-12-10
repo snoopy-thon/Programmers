402f1e90526adb2e9a77fcb9819b0957a24d3a3e
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2023-12-10 18:10:34 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.34 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        // 주어진 배열을 오름차순으로 정렬한다.
-        // 길이가 10이 아니라면,
-        // numbers[i]
-        // 주어진 배열의 길이 ~ 9까지 더한다.
+     
         int answer = 0;
-        //0,4,5,6,7,8,9
+
         int[] originArray = {0,1,2,3,4,5,6,7,8,9};
         Arrays.sort(numbers);
+        
         int i = 0, j = 0;
         while(i < numbers.length){
             if(numbers[i] != originArray[j]){
