abaae0fac66aa470906426dc5d150871662e7497
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-01-12 20:41:31 +0900

    [level Lv.1] Title: 문자열 내림차순으로 배치하기, Time: 0.58 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        StringBuffer sb = new StringBuffer(new String(arr));
+        String answer = sb.reverse().toString();
+        
+        return answer;
+    }
+}
