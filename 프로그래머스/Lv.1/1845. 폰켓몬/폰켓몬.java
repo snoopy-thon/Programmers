d0ff01ab9c74c87d9d0ca1eec6fa93df57e75891
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-01-16 15:37:03 +0900

    [level Lv.1] Title: 폰켓몬, Time: 4.95 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.java
@@ -1,21 +1,18 @@
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 class Solution {
     public int solution(int[] nums) {
+        // 1. nums.length / 2 만큼 뽑을 수 있음
+        // 2. nums에서 중복 제거하기
+        // 3-1. 중복제거한 배열의 크기 >= nums.length / 2 라면
+        //      answer = nums.length / 2
+        // 3-2. 중복제거한 배열의 크기 < nums.length / 2 라면
+        //      answer = 중복제거한 배열의 크기
         int answer = 0;
-        // nums.length / 2 만큼 뽑을 수 있음
-        // nums에서 중복 제거하기
-        // 만약 중복제거한 배열의 크기 >= nums.length / 2 라면
-        //  answer = nums.length / 2
-        // 만약 중복제거한 배열의 크기 < nums.length / 2 라면
-        //  answer = 중복제거한 배열의 크기
-        
         int[] arr = Arrays.stream(nums).distinct().toArray();
         if(arr.length >= nums.length / 2){
             answer = nums.length / 2;
-        }else{
+        } else{
             answer = arr.length;
         }
         return answer;
