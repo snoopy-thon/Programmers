a3db3bc0fdd13fa5f4cacaee517176cf8cd0b0ab
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-05-15 08:58:19 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 116 ms, Memory: 43,708 KB -BaekjoonHub

--- SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.py
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.py
@@ -41,9 +41,11 @@ T = int(input())
 for test_case in range(1, T + 1):
     a, b = map(int, input().split())
     if a > b:
-        print(f"#{test_case} >")
+        result = ">"
     elif a < b:
-        print(f"#{test_case} <")
+        result = "<"
     else :
-        print(f"#{test_case} =")
+        result = "="
+    print(f"#{test_case} {result}")
+    
     
