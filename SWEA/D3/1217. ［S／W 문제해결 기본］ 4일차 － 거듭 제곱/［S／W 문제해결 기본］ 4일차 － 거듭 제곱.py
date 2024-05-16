da62ff9854f7b59b96430e436465351115f15de0
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-05-16 22:16:58 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 105 ms, Memory: 43,704 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,6 +1,12 @@
+def recursion_func(n, m):
+    if m == 0:
+    	return 1
+    else:
+        return n * recursion_func(n,m-1)
 T = 10
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     test_num = int(input())
     n, m = map(int, input().split())
-    print(f"#{test_case} {n ** m}")
+    answer = recursion_func(n, m)
+    print(f"#{test_case} {answer}")
