7abc69877f8164fd1f56a9dff774de1cc15bce4e
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-05-18 16:05:11 +0900

    [D2] Title: 조교의 성적 매기기, Time: 118 ms, Memory: 46,396 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -2,6 +2,12 @@ T = int(input())
 
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
+    # n명의 학생, 학점을 알고싶은 k번째 학생
+    # 학생들의 각각 총점 구하기
+	# 총점을 내림차순 정렬
+	# k번째 학생의 등수 구하기
+	# grade[k번째 학생의 등수 // (n / 10)] 
+    
     total_score = []
     grade = ["A+","A0","A-","B+","B0","B-","C+","C0","C-","D0"]
     n, k = map(int, input().split())
