bd3001074cc4ad5df7c9893d4f0eebef15f47acd
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-05-21 17:27:25 +0900

    [goormlevel] Title: A + B (2), Time: 0.14 ms, Memory: 34.59 MB, Difficulty: 1 -BaekjoonHub

---
+++ goormlevel/174704/1. A ＋ B （2）/A ＋ B （2）.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		double a = Double.parseDouble(st.nextToken());
+		double b = Double.parseDouble(st.nextToken());
+		
+		double answer = a + b;
+		System.out.printf("%.6f",answer);
+	}
+}
