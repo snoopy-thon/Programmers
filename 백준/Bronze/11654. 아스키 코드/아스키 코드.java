24e6af9eebaacce7e41207c83a840e4bfadb0a76
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-05-23 20:08:02 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char input = br.readLine().charAt(0);
+        int num = (int)input;
+        System.out.println(num);
+
+    }
+}
+
+
+
+
+
+
