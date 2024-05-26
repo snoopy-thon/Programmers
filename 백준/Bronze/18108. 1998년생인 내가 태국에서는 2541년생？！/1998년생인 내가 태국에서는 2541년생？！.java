b3591c8d5546e3f2cbde5ff4e0264b936b9e7394
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-05-27 00:02:46 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        System.out.println(input - 543);
+
+        }
+	}
