d1d67383648053d3e8d73ce7aef1cfc2d0733491
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-05-27 00:03:43 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input + "??!");
+
+        }
+
+
+
+	}
