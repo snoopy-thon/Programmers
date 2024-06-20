9231fd10e13f212e70708470f574776ee1bb226b
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-06-20 10:04:43 +0900

    [level 2] Title: 올바른 괄호, Time: 8.63 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,33 +2,22 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        Stack<Character> st = new Stack();
-        int right_count=0;
-        int left_count=0;
-        
-        if(s.charAt(0) == ')'|| s.charAt(s.length()-1)=='('){
-            answer = false;
-            return answer;
-        }else{
-            for(int i=0; i<s.length(); i++ ){
-                st.push(s.charAt(i));
+       boolean answer = false;
+        int count = 0;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
             }
-            while(!st.isEmpty()){
-            if(st.peek()==')'){
-                st.pop();
-                right_count++;
-            }else if(st.peek()=='('){
-                st.pop();
-                left_count++;
-                if(left_count > right_count){
-                    answer = false;
-                    return answer;
-                }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
             }
         }
+        if(count == 0){
+            answer = true;
         }
-
 
         return answer;
     }
