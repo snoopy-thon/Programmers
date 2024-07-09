01ce7a6635158c47a980d4093f9bccbd20d2cf14
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-07-09 18:51:50 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,32 +2,46 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        // 1. 공백을 만나면 인덱스를 0으로 초기화, 공백 추가
-        // 2. 인덱스가 0이고 a~z일 경우, 대문자로 변환하고 대체한다.
-        // 3. 인덱스가 0이 아닌 나머지 경우, 소문자로 변환하고 대체한다.
+        // 1. 공백을 만나면 인덱스를 0으로 초기화
+        // 2. 인덱스가 1이고 소문자인 경우, 대문자로 변환한다.
+        // 3. 인덱스가 1이 아니고 대문자인 경우, 소문자로 변환한다.
        
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         int index = 1;
+        
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == ' '){
+            char currentChar = s.charAt(i);
+            
+            if(currentChar == ' '){
                 index = 0;
+                answer.append(currentChar);
+                index++;
+                continue;
             }
+            
             if(index == 1){
-                if(s.charAt(i) >= 'a' && s.charAt(i) <='z'){
-                    answer += (char)(s.charAt(i) - 32);
-                }else{
-                    answer += s.charAt(i);
-                }
+                answer.append(changeToUpper(currentChar));
             }else {
-                if(s.charAt(i) >= 'A' && s.charAt(i) <='Z'){
-                    answer += (char)(s.charAt(i) + 32);
-                }else{
-                    answer += s.charAt(i);
-                }
+                answer.append(changeToLower(currentChar));
             }
             index++;
         }
-
-        return answer;
-        }
+        return answer.toString();
     }
+    
+    // 대문자 변환 함수
+    public char changeToUpper(char c){
+        if(c >= 'a' && c <='z'){
+            return (char)(c - 32);
+        }
+        return c;
+    }
+    
+    // 소문자 변환 함수
+    public char changeToLower(char c){
+        if(c >= 'A' && c <='Z'){
+            return (char)(c + 32);
+        }
+        return c;
+    }
+}
