78af5edd88d63684135885d5821d7210bdf2c2ce
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-08-30 16:04:14 +0900

    [level 1] Title: K번째수, Time: 0.59 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,21 +1,22 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        // Arrays.copyOfRange() 사용한 풀이
         // array를 i부터 j까지 잘라서 다른 배열에 옮긴다.
         // 새로운 array를 정렬한다.
         // k번째 인덱스에 해당하는 새로운 array값을 answer배열에 넣는다.
         int[] answer = new int[commands.length];
-        int j=0;
-        for(int[] command : commands){
-            int length = command[1] - command[0] + 1;
-            int[] newArray = new int[length];
-            for(int i=0; i<length; i++){
-                newArray[i] = array[command[0]-1+i];
-            }
+        int index = 0;
+        
+        for(int[] command:commands){
+            int i = command[0];
+            int j = command[1];
+            int k = command[2];
+            int[] newArray = Arrays.copyOfRange(array,i-1,j);
             Arrays.sort(newArray);
-            answer[j] = newArray[command[2]-1];
-            j++;
+            answer[index] = newArray[k-1];
+            index++;
         }
         
         return answer;
