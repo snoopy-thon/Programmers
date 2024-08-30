20d38479a88f4983bf26f39d2f7113667cedbd4e
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2024-08-30 15:31:51 +0900

    [level 1] Title: 최소직사각형, Time: 1.92 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,25 +1,11 @@
 class Solution {
     public int solution(int[][] sizes) {
-        // 최소 직사각형
-        // 가로와 세로 둘중 더 긴 길이를 가로에 넣는다.
-        // 가로 중 가장 긴 길이 * 세로 중 가장 긴 길이
         int answer = 0;
-        for(int i=0; i<sizes.length; i++){
-            if(sizes[i][0] < sizes[i][1]){
-                int tmp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = tmp;
-            }
-        }
-        int maxW = sizes[0][0];
-        int maxH = sizes[0][1];
-        for(int j=0; j<sizes.length; j++){
-            if(sizes[j][0] > maxW){
-                maxW = sizes[j][0];
-            }
-            if(sizes[j][1] > maxH){
-                maxH = sizes[j][1];
-            }
+        int maxW = 0;
+        int maxH = 0;
+        for(int[] card : sizes){
+            maxW = Math.max(maxW, Math.max(card[0], card[1]));
+            maxH = Math.max(maxH, Math.min(card[0], card[1]));
         }
         answer = maxW * maxH;
         return answer;
